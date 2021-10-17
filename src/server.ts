b953472d90259b7a2b3d2a5ec5ecb458faaa72ec
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-10-17 17:45:30 -0300

    chore: configure debug tools

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,18 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: true });
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
 app.listen(3333, () => {
   console.log('🚀 Server is running at http://localhost:3333');
 });
