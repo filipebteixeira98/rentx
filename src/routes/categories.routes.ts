4860963aee7769ea5273e262d3cf44ffa0366576
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-03-27 18:24:18 -0300

    chore: implemented categories routes

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.get('/', (request, response) => {
+  const categories = categoriesRepository.index();
+
+  return response.json(categories);
+});
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response
+      .status(400)
+      .json({ error: 'Category with such name already exists!' });
+  }
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
