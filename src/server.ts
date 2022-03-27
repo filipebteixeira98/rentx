4ab229eba5675f221775c7d9ba7e45f81db31bc1
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-03-27 18:25:13 -0300

    feat: included route to categories actions

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: true });
-});
-
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => {
   console.log('🚀 Server is running at http://localhost:3333');
